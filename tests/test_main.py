b30a00017f16e45b0fa88bfec21e72ac4c3cf5aa
Andrii Dukhvin <andreysypertop@gmail.com>
2026-02-26 01:58:30 +0000

    test: add pytest:

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+from httpx import AsyncClient
+from app.main import app
+import os
+
+@pytest.mark.asyncio
+async def test_root_endpoint():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/")
+    
+    assert response.status_code == 200
+    
+    data = response.json()
+    assert "greetings" in data
+    assert isinstance(data["greetings"], list)
