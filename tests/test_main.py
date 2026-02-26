b6358077cb8a6e4b9743e840306fa3c61d3414cb
Andrii Dukhvin <andreysypertop@gmail.com>
2026-02-26 02:04:17 +0000

    fix: add init files and fix pythonpath for tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,11 @@
 import pytest
 from httpx import AsyncClient
-from app.main import app
+from app.main import server
 import os
 
 @pytest.mark.asyncio
 async def test_root_endpoint():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    async with AsyncClient(app=server, base_url="http://test") as ac:
         response = await ac.get("/")
     
     assert response.status_code == 200
