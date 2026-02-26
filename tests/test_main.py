e43b8f5a7815abc035b135d4a988440354ff3b76
Andrii Dukhvin <andreysypertop@gmail.com>
2026-02-26 02:10:14 +0000

    fix: update httpx AsyncClient syntax to use ASGITransport

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,14 @@
 import pytest
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 from app.main import server
-import os
 
 @pytest.mark.asyncio
 async def test_root_endpoint():
-    async with AsyncClient(app=server, base_url="http://test") as ac:
+
+    transport = ASGITransport(app=server)
+    
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
         response = await ac.get("/")
     
     assert response.status_code == 200
-    
-    data = response.json()
-    assert "greetings" in data
-    assert isinstance(data["greetings"], list)
+    assert "greetings" in response.json()
